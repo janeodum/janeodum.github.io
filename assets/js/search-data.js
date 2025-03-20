c1e17db412984b1044e7632d4983dfb3efe0cfff
janeodum <janeodum41@gmail.com>
2025-03-20 16:55:25 +0000

    Deploying to gh-pages from @ janeodum/janeodum.github.io@c1d95eee38f0f3a385ae0d83f4028e0640c9b10a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,13 +51,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/dropdown/";
-          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
